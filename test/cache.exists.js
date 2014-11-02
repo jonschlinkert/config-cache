bdcb852a8d14999defd77bfda90a9f516dfd90a3
jonschlinkert <dev@sellside.com>
2014-11-01 23:50:06 -0400

    split out tests

---
+++ test/cache.exists.js
@@ -0,0 +1,56 @@
+/*!
+ * config-cache <https://github.com/jonschlinkert/config-cache>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var Config = require('..');
+var config;
+var obj;
+
+describe('exists():', function () {
+  beforeEach(function () {
+    config = new Config();
+    obj = {a: {b: {c: 1, d: '', e: null, f: undefined, 'g.h.i': 2}}};
+    config.merge(obj);
+  })
+
+  it('immediate property should exist.', function() {
+    config.exists('a').should.be.true;
+  });
+  it('nested property should exist.', function() {
+    config.exists('a.b').should.be.true;
+  });
+  it('nested property should exist.', function() {
+    config.exists('a.b.c').should.be.true;
+  });
+
+  it.skip('nested property should exist.', function() {
+    config.exists('a.b.d').should.be.true;
+  });
+  it.skip('nested property should exist.', function() {
+    config.exists('a.b.e').should.be.true;
+  });
+  it.skip('nested property should exist.', function() {
+    config.exists('a.b.f').should.be.true;
+  });
+
+  it('literal backslash should escape period in property name.', function() {
+    config.exists('a.b.g\\.h\\.i').should.be.true;
+  });
+
+  it('nonexistent property should not exist.', function() {
+    config.exists('x').should.eql(false);
+  });
+  it('nonexistent property should not exist.', function() {
+    config.exists('a.x').should.eql(false);
+  });
+  it('nonexistent property should not exist.', function() {
+    config.exists('a.b.x').should.eql(false);
+  });
+});
---
+++ test/cache.get.js
@@ -0,0 +1,61 @@
+/*!
+ * config-cache <https://github.com/jonschlinkert/config-cache>
+ *
+ * Copyright (c) 2014 Jon Schlinkert, Brian Woodward, contributors.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var Config = require('..');
+var config;
+var obj;
+
+describe('.get()', function () {
+  beforeEach(function () {
+    config = new Config();
+    obj = {a: {b: {c: 1, d: '', e: null, f: undefined, 'g.h.i': 2}}};
+    config.merge(obj);
+  })
+  describe('get() - add:', function () {
+
+    it('should get immediate properties.', function() {
+      config.get('a').should.eql(obj.a);
+    });
+    it('should get nested properties.', function() {
+      config.get('a.b').should.eql(obj.a.b);
+    });
+    it('should return undefined for nonexistent properties.', function() {
+      (typeof config.get('a.x')).should.be.undefined;
+    });
+    it('should return values.', function() {
+      config.get('a.b.c').should.eql(1);
+    });
+    it('should return values.', function() {
+      config.get('a.b.d').should.eql('');
+    });
+    it('should return values.', function() {
+      (typeof config.get('a.b.e')).should.be.an.object;
+      (config.get('a.b.e') == null).should.be.true;
+    });
+    it('should return values.', function() {
+      (typeof config.get('a.b.f')).should.be.undefined;
+    });
+    it('literal backslash should escape period in property name.', function() {
+      config.get('a.b.g\\.h\\.i').should.equal(2);
+    });
+    it('should just return existing properties.', function() {
+      config.get('a', true).should.eql(config.cache.a);
+    });
+    it('should create immediate properties.', function() {
+      config.get('b', true).should.eql(config.cache.b);
+    });
+    it('should create nested properties.', function() {
+      config = new Config();
+      config.get('c.d.e', true);
+      config.cache.c.d.e.should.equal(true);
+    });
+  });
+});
