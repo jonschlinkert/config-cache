e9874c4725c2ff2de676dc18351156943208ed96
Brian Woodward <brian.woodward@gmail.com>
2014-11-04 15:31:30 -0500

    fixing omit
    arguments passed to `hasOwn` were backwards. fixes and addes tests to ensure keys aren't removed when they shouldn't be

--- test/cache.omit.js
+++ test/cache.omit.js
@@ -21,13 +21,16 @@ describe('config omit', function () {
 
     it('should omit a value from the cache', function () {
       config.set('a', 'b');
+      config.set('b', 'c');
 
       // property should be on the cache
       config.get('a').should.equal('b');
+      config.get('b').should.equal('c');
       config.omit('a');
 
       // property should not be on the cache
       (config.get('a') == null).should.be.true;
+      (config.get('b') === 'c').should.be.true;
     });
 
     it('should omit an array of values from the cache', function () {
@@ -35,13 +38,15 @@ describe('config omit', function () {
       	.set('a', 'a')
       	.set('b', 'b')
       	.set('c', 'c')
-      	.set('d', 'd');
+      	.set('d', 'd')
+        .set('e', 'e');
 
       // properties should be on the cache
       config.get('a').should.equal('a');
       config.get('b').should.equal('b');
       config.get('c').should.equal('c');
       config.get('d').should.equal('d');
+      config.get('e').should.equal('e');
 
       config.omit(['a', 'b', 'c', 'd']);
 
@@ -50,6 +55,7 @@ describe('config omit', function () {
       (config.get('b') == null).should.be.true;
       (config.get('c') == null).should.be.true;
       (config.get('d') == null).should.be.true;
+      (config.get('e') === 'e').should.be.true;
     });
 
   });
